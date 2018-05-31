251c251fb9bed46154f434d2865e958534038dfc
mac <wsw622@126.com>
2018-05-31 23:20:28 +0800

    service

--- app/src/main/java/com/aliouswang/battle/MainActivity.java
+++ app/src/main/java/com/aliouswang/battle/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.aliouswang.battle.service.MyConnection;
 import com.aliouswang.battle.service.MyService;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,11 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
         final Intent service = new Intent(this, MyService.class);
         startService(service);
+        startService(service);
+
+        final MyConnection connection = new MyConnection();
+        bindService(service, connection, BIND_AUTO_CREATE);
+
+        final MyConnection connection2 = new MyConnection();
+        bindService(service, connection2, BIND_AUTO_CREATE);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 stopService(service);
+                stopService(service);
+                unbindService(connection2);
             }
         }, 3000);
     }
--- app/src/main/java/com/aliouswang/battle/service/MyService.java
+++ app/src/main/java/com/aliouswang/battle/service/MyService.java
@@ -2,6 +2,7 @@ package com.aliouswang.battle.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
@@ -17,24 +18,34 @@ public class MyService extends Service{
     @Override
     public IBinder onBind(Intent intent) {
         L.e("onBind");
-        return null;
+        MyBinder binder = new MyBinder();
+        L.e("onBind thread : " + Thread.currentThread().getName());
+        return binder;
+//        return null;
     }
 
     @Override
     public void onCreate() {
         L.e("onCreate");
+        L.e("onCreate thread : " + Thread.currentThread().getName());
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         L.e("onStartCommand");
+        L.e("onStartCommand thread : " + Thread.currentThread().getName());
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         L.e("onDestory");
+        L.e("onDestroy thread : " + Thread.currentThread().getName());
         super.onDestroy();
+    }
+
+    private class MyBinder extends Binder {
+
     }
 }
